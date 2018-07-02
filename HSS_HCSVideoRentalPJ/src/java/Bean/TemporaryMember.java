80c1f97f1db4b633eb973b6ec6c1a80ee4f11f91
s20163002 <s20163002@HCS23103.stdlan.hcs.local>
2018-07-02 14:24:22 +0900

    Beanフォルダ作成

--- HSS_HCSVideoRentalPJ/src/java/Bean/TemporaryMember.java
+++ HSS_HCSVideoRentalPJ/src/java/Bean/TemporaryMember.java
@@ -7,8 +7,19 @@ package Bean;
 
 /**
  *
- * @author s20163002
+ * @author Gura3
  */
 public class TemporaryMember {
-    
+    private String memberno;        //仮会員No
+    private String birthday;        //生年月日
+    private String name;            //氏名
+    private String kana;            //会員名(フリガナ)
+    private String sex;             //性別
+    private String postal;          //郵便番号
+    private String address;         //住所
+    private String phone;           //電話番号
+    private String occupation;      //職業
+    private String mail_address;    //メールアドレス
+    private String mail_magazine;   //メールマガジン
+    private String registararion_date;
 }
